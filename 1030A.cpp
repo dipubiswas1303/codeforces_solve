a595d60b581d329aa2de0472bf9cb3db04781976
Dipu Biswas <dipubiswas1303@gmail.com>
2021-05-01 20:39:22 +0600

    A. In Search of an Easy Problem
    https://codeforces.com/problemset/problem/1030/A

--- 1030A.cpp
+++ 1030A.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 
-/*DIPU BISWAS 1030A*/
-
 using namespace std;
+
+   /*
+   NAME : DIPU BISWAS
+   JUST CSE 2019 - 2020
+   PROBLEM CODE : 116A
+   LINK : https://codeforces.com/problemset/problem/1030/A
+   */
 
 int main()
 {
