89ad1ca75d638f3cd9f7c56202edf21158b68ac4
Dipu Biswas <dipubiswas1303@gmail.com>
2021-05-01 22:10:28 +0600

    A. Petya and Strings
    https://codeforces.com/problemset/problem/112/A

--- 112A.cpp
+++ 112A.cpp
@@ -3,6 +3,13 @@
 #include <cstring>
 using namespace std;
 
+   /*
+   NAME : DIPU BISWAS
+   JUST CSE 2019 - 2020
+   PROBLEM CODE : 112A
+   LINK : https://codeforces.com/problemset/problem/112/A
+   */
+
 int main()
 {
     char a[101], b[101];
