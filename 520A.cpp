649de5ec54bd2f975e92d3537945041a6a98f570
Dipu Biswas <dipubiswas1303@gmail.com>
2021-05-01 23:25:20 +0600

    A. Pangram
    https://codeforces.com/problemset/problem/520/A

--- 520A.cpp
+++ 520A.cpp
@@ -2,7 +2,12 @@
 
 using namespace std;
 
-/*DIPU BISWAS 520A*/
+   /*
+   NAME : DIPU BISWAS
+   JUST CSE 2019 - 2020
+   PROBLEM CODE : 520A
+   LINK : https://codeforces.com/problemset/problem/520/A
+   */
 
 int main()
 {
