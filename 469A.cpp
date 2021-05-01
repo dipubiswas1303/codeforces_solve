af5033a6e064e1386a90381c67e0a7c04bcdd3ca
Dipu Biswas <dipubiswas1303@gmail.com>
2021-05-01 23:16:08 +0600

    A. I Wanna Be the Guy
    https://codeforces.com/problemset/problem/469/A

--- 469A.cpp
+++ 469A.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 
-/*DIPU BISWAS 469A*/
-
 using namespace std;
+
+   /*
+   NAME : DIPU BISWAS
+   JUST CSE 2019 - 2020
+   PROBLEM CODE : 469A
+   LINK : https://codeforces.com/problemset/problem/469/A
+   */
 
 int main()
 {
