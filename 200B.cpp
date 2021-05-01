78582874fbc46d501a73b7de830cfc4655645bd8
Dipu Biswas <dipubiswas1303@gmail.com>
2021-05-01 23:09:38 +0600

    B. Drinks
    https://codeforces.com/problemset/problem/200/B

--- 200B.cpp
+++ 200B.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 
-/*DIPU BISWAS 200B*/
-
 using namespace std;
+
+   /*
+   NAME : DIPU BISWAS
+   JUST CSE 2019 - 2020
+   PROBLEM CODE : 200B
+   LINK : https://codeforces.com/problemset/problem/200/B
+   */
 
 int main()
 {
