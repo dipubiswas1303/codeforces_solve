0ce243541ae119a87489713ddbd96361c4ec8a73
Dipu Biswas <dipubiswas1303@gmail.com>
2021-05-01 23:11:42 +0600

    A. Expression
    https://codeforces.com/problemset/problem/479/A

--- 479A.cpp
+++ 479A.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 
-/*DIPU BISWAS 479A*/
-
 using namespace std;
+
+   /*
+   NAME : DIPU BISWAS
+   JUST CSE 2019 - 2020
+   PROBLEM CODE : 479A
+   LINK : https://codeforces.com/problemset/problem/479/A
+   */
 
 int main()
 {
